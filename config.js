bd803463f2d6c43cb6d99d364513c8e19abf9ff0
tahabahman826 <tahabahman826@gmail.com>
2026-09-24 16:33:31 +0330

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.MUSIC_WORLD_CONFIG = Object.freeze({
     backendOrigin:
         ["localhost", "127.0.0.1"].includes(window.location.hostname)
             ? window.location.origin
-            : "https://music-world-nqo1.onrender.com"
+            : "https://music-world-2-0-upgraded.onrender.com"
 });
